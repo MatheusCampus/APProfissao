aff1947056a2ed4f0cb62a79a08f1c86ac59c754
MatheusCampus <Matheus-a-c@hotmail.com>
2021-11-08 11:21:05 -0400

    commit botao falhando....

--- APProfisso/app/src/main/java/com/example/approfisso/MainActivity.java
+++ APProfisso/app/src/main/java/com/example/approfisso/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.approfisso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.a2);
+        setContentView(R.layout.activity_main);
+
+        Button button_oferece = findViewById(R.id.button_oferece);
+
+        button_oferece.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                Intent intent = new Intent(MainActivity.this, Oferece_emprego.class );
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 }
---
+++ APProfisso/app/src/main/java/com/example/approfisso/Oferece_emprego.java
@@ -0,0 +1,19 @@
+package com.example.approfisso;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Oferece_emprego extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.empresabuscando);
+    }
+
+
+}
+
+
